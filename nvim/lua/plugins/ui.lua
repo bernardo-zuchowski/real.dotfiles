fb6075647dc61fbb507d863131a84739520ab379
Bernardo Zuchowski <ber.s.zuc@gmail.com>
2024-03-03 14:36:05 -0300

    update status line

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -37,6 +37,22 @@ return {
         component_separators = '|',
         section_separators = '',
       },
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'filename' },
+        lualine_c = { 'branch', 'diff', 'diagnostics' },
+        lualine_x = { 'encoding', 'fileformat', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' }
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { 'filename' },
+        lualine_x = { 'location' },
+        lualine_y = {},
+        lualine_z = {}
+      },
     },
   },
 

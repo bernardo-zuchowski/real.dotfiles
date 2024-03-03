116cef7365b818da8e1cee079aed5e7b9fcf5f38
Bernardo Zuchowski <ber.s.zuc@gmail.com>
2024-03-03 14:36:22 -0300

    update blankline according to release changes

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -57,14 +57,18 @@ return {
   },
 
   {
-    -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help indent_blankline.txt`
-    opts = {
-      char = '┊',
-      show_trailing_blankline_indent = false,
-    },
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    config = function()
+      require('ibl').setup({
+        scope = {
+          enabled = false,
+        },
+        indent = {
+          char = '┊',
+        },
+      })
+    end
   },
 
   {
